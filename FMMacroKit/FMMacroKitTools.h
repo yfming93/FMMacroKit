c9420fa28f86fb862d6b2afb30398ab86ffb9566
袁凤鸣 <yfmingo@163.com>
2019-12-20 19:01:47 +0800

    add：新增支持检查iPhone11

--- FMMacroKit/FMMacroKitTools.h
+++ FMMacroKit/FMMacroKitTools.h
@@ -11,7 +11,7 @@
 @interface FMMacroKitTools : NSObject
 /// 判断刘海屏，返回YES表示是刘海屏
 + (BOOL)fm_isNotchScreen;
-/// 16进制颜色转换
+/// 16进制 颜色 转换 传入 #5D9EE1，5D9EE1，0x5D9EE1 等等
 + (UIColor *)fm_colorWithHexString:(NSString *)hexStr;
 /// 是否包含中文
 + (BOOL)fm_isContainChinese:(NSString *)str;
--- FMMacroKit/FMMacroKitTools.m
+++ FMMacroKit/FMMacroKitTools.m
@@ -64,22 +64,27 @@ static BOOL fm_hexStrToRGBA(NSString *str,
 
 /// 判断刘海屏，返回YES表示是刘海屏
 + (BOOL)fm_isNotchScreen {
+    /*
+     https://kangzubin.com/iphonex-detect/
+     iPhone X 对应的 device mode 为 iPhone10,3 和 iPhone10,6，而今年最新发布 iPhone XS 对应 iPhone11,2，iPhone XS Max 对应 iPhone11,4 和 iPhone11,6，iPhone XR 对应 iPhone11,8，完整的 device mode 数据参考这里：
+     https://www.theiphonewiki.com/wiki/Models
+     */
     static BOOL isiPhoneX = NO;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
 #if TARGET_IPHONE_SIMULATOR
-        // 获取模拟器所对应的 device model
+            // 获取模拟器所对应的 device model
         NSString *model = NSProcessInfo.processInfo.environment[@"SIMULATOR_MODEL_IDENTIFIER"];
 #else
-        // 获取真机设备的 device model
+            // 获取真机设备的 device model
         struct utsname systemInfo;
         uname(&systemInfo);
         NSString *model = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 #endif
-        // 判断 device model 是否为 "iPhone10,3" 和 "iPhone10,6" 或者以 "iPhone11," 开头
-        // 如果是，就认为是 iPhone X
-        isiPhoneX = [model isEqualToString:@"iPhone10,3"] || [model isEqualToString:@"iPhone10,6"] || [model hasPrefix:@"iPhone11,"];
+            // 判断 device model 是否为 "iPhone10,3" 和 "iPhone10,6" 或者以 "iPhone11," 开头就是 iPhone X
+            // 以 "iPhone12," 开头就是 iPhone11
+        isiPhoneX = [model isEqualToString:@"iPhone10,3"] || [model isEqualToString:@"iPhone10,6"] || [model hasPrefix:@"iPhone11,"] || [model hasPrefix:@"iPhone12,"];
     });
     
     return isiPhoneX;
